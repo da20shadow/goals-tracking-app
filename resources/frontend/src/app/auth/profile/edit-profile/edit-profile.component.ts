577c933c30dfef16da035e8de916df32c41966c0
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-20 15:49:30 +0200

    edit profile component and functions done

--- resources/frontend/src/app/auth/profile/edit-profile/edit-profile.component.ts
+++ resources/frontend/src/app/auth/profile/edit-profile/edit-profile.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import {Observable} from "rxjs";
+import {User} from "../../../core/models";
+import {Store} from "@ngrx/store";
+import {UserPageActions} from "../../../Store/user-store/user-page.actions";
+import {userSelectors} from "../../../Store/user-store/user-selectors";
+import {GlobalClasses} from "../../../shared/styles/global-classes";
+import {AuthService} from "../../services/auth.service";
+import {NotificationService} from "../../../core/services/notification.service";
+import {NgForm} from "@angular/forms";
+import {UserAPIActions} from "../../../Store/user-store/user-api.actions";
 
 @Component({
   selector: 'app-edit-profile',
@@ -7,4 +17,29 @@ import { Component } from '@angular/core';
 })
 export class EditProfileComponent {
 
+  classes = GlobalClasses;
+  user$: Observable<User|null>;
+
+  constructor(private store$: Store,private authService: AuthService,
+              private notificationService: NotificationService) {
+    this.store$.dispatch(UserPageActions.getUser());
+    this.user$ = this.store$.select(userSelectors.selectUser);
+  }
+
+  editProfileFormHandler(editProfileForm: NgForm) {
+    if (editProfileForm.invalid){
+      this.notificationService.showErrorNotification('Invalid Form Fields!');
+      return;
+    }
+    this.authService.updateProfile(editProfileForm.value).subscribe({
+      next: (response) => {
+        this.notificationService.showSuccessNotification(response.message);
+        this.store$.dispatch(UserAPIActions.setUser({user:response.user}))
+      },
+      error: (err) => {
+        this.notificationService.showErrorNotification(err.error.message);
+      }
+    })
+
+  }
 }
