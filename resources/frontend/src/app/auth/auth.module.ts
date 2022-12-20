35597967eaf6adcd6accc2097908be320cf80e8e
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-20 15:48:17 +0200

    imported needed modules and export all components

--- resources/frontend/src/app/auth/auth.module.ts
+++ resources/frontend/src/app/auth/auth.module.ts
@@ -1,21 +1,37 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { LogoutComponent } from './logout/logout.component';
+import {SharedModule} from "../shared/shared.module";
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {LogoutComponent} from './logout/logout.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatIconModule} from "@angular/material/icon";
+import {ProfileComponent} from "./profile/profile.component";
+import {EditProfileComponent} from "./profile/edit-profile/edit-profile.component";
 
 
 @NgModule({
   declarations: [
     LoginComponent,
     RegisterComponent,
-    LogoutComponent
+    LogoutComponent,
+    ProfileComponent,
+    EditProfileComponent
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
+    MatIconModule,
+  ],
+  exports: [
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    EditProfileComponent
   ]
 })
-export class AuthModule { }
+export class AuthModule {
+}
