ab461be07640201aac7b3ab50cf2de1504f6d34d
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-20 15:47:11 +0200

    updated router

--- resources/frontend/src/app/app-routing.module.ts
+++ resources/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {FaqComponent} from "./pages/faq/faq.component";
+import {ContactUsComponent} from "./pages/contact-us/contact-us.component";
+import {NotFoundComponent} from "./pages/not-found/not-found.component";
+import {LoginComponent} from "./auth/login/login.component";
+import {RegisterComponent} from "./auth/register/register.component";
+import {ProfileComponent} from "./auth/profile/profile.component";
+import {isLoggedInGuardFn} from "./auth/guards/isLoggedInGuardFn";
+import {EditProfileComponent} from "./auth/profile/edit-profile/edit-profile.component";
+import {LogoutComponent} from "./auth/logout/logout.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch:'full'},
+  {path: 'faq', component: FaqComponent},
+  {path: 'contact-us', component: ContactUsComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {
+    path: 'profile',component: ProfileComponent,
+    canActivate: [isLoggedInGuardFn],
+    canLoad: [isLoggedInGuardFn]
+  },
+  {
+    path: 'profile/edit', component: EditProfileComponent,
+    canActivate: [isLoggedInGuardFn],
+    canLoad: [isLoggedInGuardFn]
+  },
+  {
+    path: 'logout', component: LogoutComponent,
+    canActivate: [isLoggedInGuardFn]
+  },
+  {
+    path: 'goals',loadChildren: () =>
+      import('./goals/goals.module').then(m => m.GoalsModule),
+    canActivate: [isLoggedInGuardFn],
+    canLoad: [isLoggedInGuardFn]
+  },
+  {path: '**', component: NotFoundComponent, pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
