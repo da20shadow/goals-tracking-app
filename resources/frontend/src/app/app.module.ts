2185bcd4261dd0a0fca156c48aa957c85207694d
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-20 15:47:40 +0200

    added store and effects

--- resources/frontend/src/app/app.module.ts
+++ resources/frontend/src/app/app.module.ts
@@ -1,17 +1,25 @@
-import { NgModule, isDevMode } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
-import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {JWTInterceptorService} from "./auth/interceptors/jwt-interceptor";
 import {APP_BASE_HREF} from "@angular/common";
 import {AppStateModule} from "./Store/app.state";
 import {extModules} from "./build-specifics";
+import {CoreModule} from "./core/core.module";
+import {PagesModule} from "./pages/pages.module";
+import {SharedModule} from "./shared/shared.module";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AuthModule} from "./auth/auth.module";
+import {UserApiEffects} from "./Store/user-store/user-api.effects";
+import {GoalApiEffects} from "./Store/goals-store/goals-api.effects";
+import {MatDialogModule} from "@angular/material/dialog";
 
 @NgModule({
   declarations: [
@@ -19,13 +27,24 @@ import {extModules} from "./build-specifics";
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    MatDialogModule,
+    ReactiveFormsModule,
     AppStateModule,
     StoreModule.forRoot({}, {}),
-    EffectsModule.forRoot([]),
+    EffectsModule.forRoot([
+      UserApiEffects,
+      GoalApiEffects,
+    ]),
     StoreRouterConnectingModule.forRoot(),
     extModules,
+    CoreModule,
+    PagesModule,
+    SharedModule,
+    AuthModule,
   ],
   providers: [
     {
@@ -37,4 +56,5 @@ import {extModules} from "./build-specifics";
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
