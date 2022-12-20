661120aa6645b9ae9906ad3e976964640db3c029
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-20 15:50:14 +0200

    register component done

--- resources/frontend/src/app/auth/register/register.component.ts
+++ resources/frontend/src/app/auth/register/register.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import {GlobalClasses} from "../../shared/styles/global-classes";
+import {Title} from "@angular/platform-browser";
+import {Store} from "@ngrx/store";
+import {NotificationService} from "../../core/services/notification.service";
+import {UserAPIActions} from "../../Store/user-store/user-api.actions";
+import {FormBuilder, Validators} from "@angular/forms";
+import {emailValidator} from "../../shared/validators/email.validator";
+import {sameValueValidator} from "../../shared/validators/same-value.validator";
+import {AuthService} from "../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -6,5 +16,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+
+  classes = GlobalClasses;
+
+  regForm = this.fb.group({
+    firstName: ['', [Validators.required,Validators.minLength(2)]],
+    email: ['', [Validators.required, emailValidator()]],
+    pass: this.fb.group({
+
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      rePassword: ['', [Validators.required, Validators.minLength(8)]],
+
+    },{validators: [sameValueValidator("password","rePassword")]})
+  })
+
+  constructor(private titleService: Title,
+              private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router,
+              private notificationService: NotificationService,
+              private store$: Store) {
+    this.titleService.setTitle('Register - GoalsApp');
+  }
+
+  ngOnInit() {
+  }
+
+  registerHandler() {
+    if (this.regForm.invalid) {
+      this.notificationService.showErrorNotification("Invalid Form Fields!")
+      return;
+    }
+    const regForm = this.regForm.value;
+    this.authService.register(regForm).subscribe({
+      next: (response) => {
+        this.notificationService.showSuccessNotification(response.message);
+        this.store$.dispatch(UserAPIActions.loginSuccess({user:response.user}));
+        this.router.navigate(['profile']);
+      },
+      error: (err) => {
+        this.notificationService.showErrorNotification(err.error.message);
+      }
+    })
+  }
 
 }
