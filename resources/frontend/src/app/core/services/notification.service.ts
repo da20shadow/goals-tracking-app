1a798a5596e39fc89b45154e4a5bd4245582304d
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-20 15:52:26 +0200

    created notification service

---
+++ resources/frontend/src/app/core/services/notification.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {MatSnackBar, MatSnackBarConfig} from "@angular/material/snack-bar";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  constructor(private snackBar: MatSnackBar) {
+  }
+
+  showSuccessNotification(message: string) {
+    this.snackBar.open(message, '', this.setConfig('success'))
+  }
+
+  showErrorNotification(message: string) {
+    this.snackBar.open(message, '', this.setConfig('error'))
+  }
+
+  setConfig(type: string) {
+    const style = type === 'success'
+      ? 'bg-green-600'
+      : 'bg-rose-600'
+    const config: MatSnackBarConfig = {
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      duration: 3000,
+      panelClass: [style]
+    }
+    return config;
+  }
+}
