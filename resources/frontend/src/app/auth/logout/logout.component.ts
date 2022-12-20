9004b3d6d9f11c639d343970e7023da23c949838
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-20 15:49:17 +0200

    logout done!

--- resources/frontend/src/app/auth/logout/logout.component.ts
+++ resources/frontend/src/app/auth/logout/logout.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {UserPageActions} from "../../Store/user-store/user-page.actions";
+import {Store} from "@ngrx/store";
 
 @Component({
   selector: 'app-logout',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./logout.component.scss']
 })
 export class LogoutComponent {
+
+  constructor(private store$: Store) {
+    this.store$.dispatch(UserPageActions.logout());
+  }
 
 }
