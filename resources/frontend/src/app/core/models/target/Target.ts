95673f03bc3a58e8042b39ac447bae1348ce3fba
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-21 19:21:43 +0200

    added goal id and  created at in Target model

--- resources/frontend/src/app/core/models/target/Target.ts
+++ resources/frontend/src/app/core/models/target/Target.ts
@@ -2,7 +2,9 @@ export interface Target {
   id: number;
   title: string;
   description: string;
+  created_at: string;
   total_tasks: number;
   total_completed_tasks: number | string;
   progress: number;
+  goal_id: number;
 }
