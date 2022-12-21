2e4083b089a8a1adda2bee2a1ef9deaa286a0e93
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-21 19:25:33 +0200

    added goal api actions

--- resources/frontend/src/app/Store/goals-store/goal-api.actions.ts
+++ resources/frontend/src/app/Store/goals-store/goal-api.actions.ts
@@ -1,18 +1,18 @@
 import {createAction, props} from "@ngrx/store";
 import {Goal} from "../../core/models";
 
-const loadGoalsSuccess = createAction('[GOALS LIST PAGE] Load Goals Success',props<{goals: Goal[]}>());
-const loadGoalsFailure = createAction('[GOALS LIST PAGE] Load Goals Error',props<{error: string}>());
+const loadGoalsSuccess = createAction('[GOALS LIST API] Load Goals Success',props<{goals: Goal[]}>());
+const loadGoalsFailure = createAction('[GOALS LIST API] Load Goals Failure',props<{error: string}>());
 
-const addNewGoalSuccess = createAction('[GOALS LIST PAGE] Add Goal Success', props<{ goal: Goal }>());
-const addNewGoalFailure = createAction('[GOALS LIST PAGE] Add Goal Success', props<{ error: string }>());
+const addNewGoalSuccess = createAction('[GOALS LIST API] Add Goal Success', props<{ goal: Goal }>());
+const addNewGoalFailure = createAction('[GOALS LIST API] Add Goal Failure', props<{ error: string }>());
 
-const updateActiveGoalSuccess = createAction('[GOAL DETAILS PAGE] Edit Active Goal Success', props<{ goal: Goal }>());
-const updateActiveGoalFailure = createAction('[GOAL DETAILS PAGE] Edit Active Goal Success', props<{ error: string}>());
-const removeActiveGoalSuccess = createAction('[GOAL DETAILS PAGE] Delete Active Goal Success', props<{ goalId: number }>());
+const updateActiveGoalSuccess = createAction('[GOAL DETAILS API] Update Active Goal Success', props<{ goal: Goal }>());
+const updateActiveGoalFailure = createAction('[GOAL DETAILS API] Update Active Goal Failure', props<{ error: string}>());
+const removeActiveGoalSuccess = createAction('[GOAL DETAILS API] Delete Active Goal Success', props<{ goalId: number }>());
 
-const setActiveGoalSuccess = createAction('[GOAL DETAILS PAGE] Set Active Goal Success', props<{ goal: Goal }>());
-const setActiveGoalFailure = createAction('[GOAL DETAILS PAGE] Set Active Goal Success', props<{ error: string }>());
+const setActiveGoalSuccess = createAction('[GOAL DETAILS API] Set Active Goal Success', props<{ goal: Goal }>());
+const setActiveGoalFailure = createAction('[GOAL DETAILS API] Set Active Goal Failure', props<{ error: string }>());
 
 
 export const GoalsAPIActions = {
