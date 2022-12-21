a771a273bbf0f78509db63b51a54d51139b63982
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-21 19:24:45 +0200

    imported modules

--- resources/frontend/src/app/shared/shared.module.ts
+++ resources/frontend/src/app/shared/shared.module.ts
@@ -1,17 +1,30 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PageTitleComponent} from './components/page-title/page-title.component';
+import { TasksListComponent } from './components/tasks-list/tasks-list.component';
+import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSortModule} from "@angular/material/sort";
 
 
 @NgModule({
   declarations: [
-    PageTitleComponent
+    PageTitleComponent,
+    TasksListComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatSortModule,
+    FormsModule,
+    RouterModule,
   ],
   exports: [
-    PageTitleComponent
+    PageTitleComponent,
+    TasksListComponent
   ],
 })
 export class SharedModule {
