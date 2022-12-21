96c3f6d11ada0fd3a69554003d7a056cd8ca783f
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-21 19:23:15 +0200

    Created and implemented target list

---
+++ resources/frontend/src/app/goals/components/targets-list/targets-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TargetsListComponent } from './targets-list.component';
+
+describe('TargetsListComponent', () => {
+  let component: TargetsListComponent;
+  let fixture: ComponentFixture<TargetsListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TargetsListComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TargetsListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ resources/frontend/src/app/goals/components/targets-list/targets-list.component.ts
@@ -0,0 +1,77 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Target} from "../../../core/models";
+import {GlobalClasses} from "../../../shared/styles/global-classes";
+import {NotificationService} from "../../../core/services/notification.service";
+import {Store} from "@ngrx/store";
+import {NgForm} from "@angular/forms";
+import {animate, state, style, transition, trigger} from "@angular/animations";
+import {TargetService} from "../../../targets/services/target.service";
+import {TargetApiActions} from "../../../Store/tartgets-store/target-api.actions";
+import {Observable} from "rxjs";
+import {targetSelectors} from "../../../Store/tartgets-store/target-selectors";
+
+@Component({
+  selector: 'app-targets-list',
+  templateUrl: './targets-list.component.html',
+  styleUrls: ['./targets-list.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({
+        transform: 'scaleX(0.01)', opacity: 0
+      })),
+      transition('void <=> *', animate(400)),
+    ]),
+  ]
+})
+export class TargetsListComponent {
+
+  targetsList$: Observable<Target[]>;
+  @Input() dailyTarget = '';
+  @Input() goalId!: number;
+  showAddTargetForm: boolean = false;
+  classes = GlobalClasses;
+  targetsDone?:number = 0;
+  totalTargets?:number = 0;
+
+  constructor(private notificationService: NotificationService,
+              private targetService: TargetService,
+              private store$: Store) {
+    this.targetsList$ = this.store$.select(targetSelectors.selectTargetsList);
+  }
+
+  ngOnChange(){
+    this.targetsList$.subscribe((targetList) => {
+      this.totalTargets = targetList.length;
+      let completed = 0;
+      targetList.map(t => {
+        if (t.progress >= 100){
+          completed++;
+        }
+      })
+      this.targetsDone = completed;
+    })
+  }
+
+  addTargetFormHandler(addTargetFrom: NgForm) {
+    if (addTargetFrom.invalid){
+        this.notificationService.showErrorNotification('Title Must be between 5 and 255 characters long!');
+      return;}
+    if (!this.goalId){
+      this.notificationService.showErrorNotification('Goal ID not set!');
+      return;
+    }
+    const formData = addTargetFrom.value;
+    formData.goal_id = this.goalId
+
+    this.targetService.saveTarget(formData).subscribe({
+      next: (response) => {
+        this.notificationService.showSuccessNotification(response.message);
+        this.store$.dispatch(TargetApiActions.addTargetSuccess({target:response.target}))
+      },
+      error: (err) => {
+        this.notificationService.showErrorNotification(err.error.message)
+      }
+    })
+  }
+
+}
