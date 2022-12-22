5951b7751d04150578415e9d1b75b2cd63712bb2
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-22 10:23:47 +0200

    added tasks routing

--- resources/frontend/src/app/tasks/tasks-routing.module.ts
+++ resources/frontend/src/app/tasks/tasks-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TaskDetailsComponent} from "./task-details/task-details.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: ':id', component: TaskDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
