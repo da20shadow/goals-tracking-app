ca689eb5675c3ffc5e24c64f9ead2535851d7b25
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-22 10:22:03 +0200

    updated task page actions

--- resources/frontend/src/app/Store/task-store/task-page.actions.ts
+++ resources/frontend/src/app/Store/task-store/task-page.actions.ts
@@ -1,6 +1,11 @@
 import {createAction, props} from "@ngrx/store";
 import { Task } from "src/app/core/models/task/Task";
 
+const getActiveTask = createAction('[TASK PAGE] Get Active Tasks',props<{taskId: number}>());
+const loadActiveTask = createAction('[TASK PAGE] Load Active Tasks',props<{taskId: number}>());
+//TODO: implement subtasks
+// const getActiveTaskList = createAction('[TASK PAGE] Get Active Tasks List',props<{targetId: number}>());
+
 const loadTargetTasks = createAction('[TASK PAGE] Load Target Tasks List',props<{targetId: number}>());
 const AddTask = createAction('[TASK PAGE] Add Task To Target',props<{targetId: number,task: Task}>());
 const updateTask = createAction('[TASK PAGE] Update Task',props<{taskId: number, changedTask: Task}>());
@@ -8,6 +13,8 @@ const deleteTask = createAction('[TASK PAGE] Delete Task',props<{taskId: number}
 const clear = createAction('[TASK PAGE] Clear');
 
 export const TaskPageActions = {
+  getActiveTask,
+  loadActiveTask,
   loadTargetTasks,
   AddTask,
   updateTask,
