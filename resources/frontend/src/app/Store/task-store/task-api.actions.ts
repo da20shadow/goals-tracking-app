bf7fbe1eea0a70c6147ab51f4433fbd3edfe6a37
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-22 10:22:34 +0200

    updated task api actions

--- resources/frontend/src/app/Store/task-store/task-api.actions.ts
+++ resources/frontend/src/app/Store/task-store/task-api.actions.ts
@@ -1,11 +1,17 @@
 import {createAction, props} from "@ngrx/store";
 import {Task} from "../../core/models";
 
+
+const loadActiveTaskSuccess = createAction('[TASK API] Load Active Task Success',props<{task: Task}>());
+const loadActiveTaskFailure = createAction('[TASK API] Load Active Task Failure',props<{error: string}>());
+
 const loadTargetTasksSuccess = createAction('[TASK API] Load Target Tasks List Success',props<{tasks: Task[]}>());
 const loadTargetTasksFailure = createAction('[TASK API] Load Target Tasks List Failure',props<{error: string}>());
 
 const addTaskSuccess = createAction('[TASK API] Add Task To Target Success',props<{targetId: number,task: Task}>());
 const AddTaskFailure = createAction('[TASK API] Add Task To Target Failure',props<{error: string}>());
+
+const updateActiveTaskSuccess = createAction('[TASK API] Update Active Task Success',props<{changedTask: Task}>());
 
 const updateTaskSuccess = createAction('[TASK API] Update Task Success',props<{changedTask: Task}>());
 const updateTaskFailure = createAction('[TASK API] Update Task Failure',props<{error: string}>());
@@ -14,6 +20,9 @@ const deleteTaskSuccess = createAction('[TASK API] Delete Task Success',props<{t
 const deleteTaskFailure = createAction('[TASK API] Delete Task Failure',props<{error: string}>());
 
 export const TaskAPIActions = {
+  loadActiveTaskSuccess,
+  loadActiveTaskFailure,
+  updateActiveTaskSuccess,
   loadTargetTasksSuccess,
   loadTargetTasksFailure,
   addTaskSuccess,
