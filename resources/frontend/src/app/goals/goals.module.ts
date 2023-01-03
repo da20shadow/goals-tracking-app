9e47fb0fb042fa91756a640aa08c5631851bd7ae
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-03 19:54:22 +0200

    goal details

--- resources/frontend/src/app/goals/goals.module.ts
+++ resources/frontend/src/app/goals/goals.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { GoalsRoutingModule } from './goals-routing.module';
-import { GoalsListComponent } from './goals-list/goals-list.component';
-import { GoalDetailsComponent } from './goal-details/goal-details.component';
+import {GoalsRoutingModule } from './goals-routing.module';
+import {GoalsListComponent } from './goals-list/goals-list.component';
+import {GoalDetailsComponent } from './goal-details/goal-details.component';
 import {MatDialogModule} from "@angular/material/dialog";
 import {MatMenuModule} from "@angular/material/menu";
 import {MatIconModule} from "@angular/material/icon";
-import { AddGoalModalComponent } from './components/add-goal-modal/add-goal-modal.component';
+import {AddGoalModalComponent } from './components/add-goal-modal/add-goal-modal.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {RouterModule} from "@angular/router";
-import { TargetsListComponent } from './components/targets-list/targets-list.component';
-import {NgxEditorModule} from "ngx-editor";
+import {TargetsListComponent } from './components/targets-list/targets-list.component';
 import {SharedModule} from "../shared/components/shared.module";
 import {CircleProgressModule} from "../shared/components/progress-bars/circle-progress/circle-progress.module";
 import {RichTextEditorModule} from "../shared/components/rich-text-editor/rich-text-editor.module";
@@ -33,7 +32,6 @@ import {RichTextEditorModule} from "../shared/components/rich-text-editor/rich-t
         ReactiveFormsModule,
         RouterModule,
         GoalsRoutingModule,
-        NgxEditorModule,
         SharedModule,
         CircleProgressModule,
         RichTextEditorModule,
