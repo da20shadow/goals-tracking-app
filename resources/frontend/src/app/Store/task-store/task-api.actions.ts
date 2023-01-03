842ad7c888d30849983563830f127ce43f2c26e7
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-03 19:58:20 +0200

    Added Task actions for urgent important tasks lists

--- resources/frontend/src/app/Store/task-store/task-api.actions.ts
+++ resources/frontend/src/app/Store/task-store/task-api.actions.ts
@@ -8,8 +8,17 @@ const loadActiveTaskFailure = createAction('[TASK API] Load Active Task Failure'
 const loadTargetTasksSuccess = createAction('[TASK API] Load Target Tasks List Success',props<{tasks: Task[]}>());
 const loadTargetTasksFailure = createAction('[TASK API] Load Target Tasks List Failure',props<{error: string}>());
 
+const loadUrgentTasksSuccess = createAction('[TASK API] Load URGENT Tasks List Success',props<{tasks: Task[]}>());
+const loadUrgentTasksFailure = createAction('[TASK API] Load URGENT Tasks List Failure',props<{error: string}>());
+
+const loadImportantTasksSuccess = createAction('[TASK API] Load IMPORTANT Tasks List Success',props<{tasks: Task[]}>());
+const loadImportantTasksFailure = createAction('[TASK API] Load IMPORTANT Tasks List Failure',props<{error: string}>());
+
 const addTaskSuccess = createAction('[TASK API] Add Task To Target Success',props<{targetId: number,task: Task}>());
 const AddTaskFailure = createAction('[TASK API] Add Task To Target Failure',props<{error: string}>());
+
+const addUrgentTaskSuccess = createAction('[TASK API] Add URGENT Task Success',props<{task: Task}>());
+const addUrgentTaskFailure = createAction('[TASK API] Add URGENT Task Failure',props<{error: string}>());
 
 const updateActiveTaskSuccess = createAction('[TASK API] Update Active Task Success',props<{changedTask: Task}>());
 const updateActiveTaskFailure = createAction('[TASK API] Update Active Task Failure',props<{error: string}>());
@@ -27,8 +36,14 @@ export const TaskAPIActions = {
   updateActiveTaskFailure,
   loadTargetTasksSuccess,
   loadTargetTasksFailure,
+  loadUrgentTasksSuccess,
+  loadUrgentTasksFailure,
+  loadImportantTasksSuccess,
+  loadImportantTasksFailure,
   addTaskSuccess,
   AddTaskFailure,
+  addUrgentTaskSuccess,
+  addUrgentTaskFailure,
   updateTaskSuccess,
   updateTaskFailure,
   deleteTaskSuccess,
