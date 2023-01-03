a91e061009a858b6233cbaab6b842be8c151aa3b
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-03 19:56:16 +0200

    updated task list module

--- resources/frontend/src/app/shared/components/tasks-list/tasks-list.module.ts
+++ resources/frontend/src/app/shared/components/tasks-list/tasks-list.module.ts
@@ -6,14 +6,22 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatSortModule} from "@angular/material/sort";
 import {MatTooltipModule} from "@angular/material/tooltip";
 import {RouterModule} from "@angular/router";
-import {FormsModule} from "@angular/forms";
-import { AddTaskModalComponent } from './add-task-modal/add-task-modal.component';
-import { ViewTaskModalComponent } from './view-task-modal/view-task-modal.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AddTaskModalComponent} from './add-task-modal/add-task-modal.component';
+import {ViewTaskModalComponent} from './view-task-modal/view-task-modal.component';
 import {MatCheckboxModule} from "@angular/material/checkbox";
-
+import {RichTextEditorModule} from "../rich-text-editor/rich-text-editor.module";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
 
 @NgModule({
-  declarations: [TasksListComponent, AddTaskModalComponent, ViewTaskModalComponent],
+  declarations: [
+    TasksListComponent,
+    AddTaskModalComponent,
+    ViewTaskModalComponent
+  ],
   imports: [
     CommonModule,
     MatMenuModule,
@@ -21,9 +29,16 @@ import {MatCheckboxModule} from "@angular/material/checkbox";
     MatSortModule,
     MatTooltipModule,
     MatCheckboxModule,
+    MatDialogModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
     RouterModule,
     FormsModule,
+    RichTextEditorModule,
   ],
+  providers:[MatDatepickerModule],
   exports: [TasksListComponent]
 })
 export class TasksListModule {
