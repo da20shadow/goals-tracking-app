1b80ce5628543a71f6cc500dedecbb5cd87102e2
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-03 19:52:40 +0200

    removed ngxeditor

--- resources/frontend/src/app/app.module.ts
+++ resources/frontend/src/app/app.module.ts
@@ -14,7 +14,6 @@ import {AppStateModule} from "./Store/app.state";
 import {extModules} from "./build-specifics";
 import {CoreModule} from "./core/core.module";
 import {PagesModule} from "./pages/pages.module";
-import {SharedModule} from "./shared/components/shared.module";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {AuthModule} from "./auth/auth.module";
 import {UserApiEffects} from "./Store/user-store/user-api.effects";
@@ -24,13 +23,11 @@ import {TargetEffects} from "./Store/tartgets-store/target.effects";
 import {TaskApiEffects} from "./Store/task-store/task-api.effects";
 import {LoadingInterceptor} from "./core/interceptors/loading.interceptor";
 import {AgendaApiEffects} from "./Store/agenda-store/agenda-api.effects";
-import {NgxEditorModule} from "ngx-editor";
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatListModule} from "@angular/material/list";
-import {LineChartModule} from "./shared/components/charts/line-chart/line-chart.module";
 
 
 
@@ -64,43 +61,7 @@ import {LineChartModule} from "./shared/components/charts/line-chart/line-chart.
     extModules,
     CoreModule,
     PagesModule,
-    SharedModule,
     AuthModule,
-    NgxEditorModule.forRoot({
-      locals: {
-        // menu
-        bold: 'Bold',
-        italic: 'Italic',
-        code: 'Code',
-        blockquote: 'Blockquote',
-        underline: 'Underline',
-        strike: 'Strike',
-        bullet_list: 'Bullet List',
-        ordered_list: 'Ordered List',
-        heading: 'Heading',
-        h1: 'Header 1',
-        h2: 'Header 2',
-        h3: 'Header 3',
-        h4: 'Header 4',
-        h5: 'Header 5',
-        h6: 'Header 6',
-        align_left: 'Left Align',
-        align_center: 'Center Align',
-        align_right: 'Right Align',
-        align_justify: 'Justify',
-        text_color: 'Text Color',
-        background_color: 'Background Color',
-        // popups, forms, others...
-        url: 'URL',
-        text: 'Text',
-        openInNewTab: 'Open in new tab',
-        insert: 'Insert',
-        altText: 'Alt Text',
-        title: 'Title',
-        remove: 'Remove',
-      },
-    }),
-    LineChartModule,
   ],
   providers: [
     {
