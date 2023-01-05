fee3d6724b57901a83a628cdb1d6809e46cc2eaf
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-05 18:20:10 +0200

    fixed bug

--- resources/frontend/src/app/shared/components/date-time-picker/date-time-picker.component.ts
+++ resources/frontend/src/app/shared/components/date-time-picker/date-time-picker.component.ts
@@ -20,8 +20,8 @@ export class DateTimePickerComponent {
     if (this.defaultDate) {
       date = new Date(this.defaultDate);
 
-      const day = date.getDate() < 9 ? '0' + (date.getDate()) : date.getDate();
-      const month = date.getMonth() < 9 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
+      const day = date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate();
+      const month = date.getMonth() < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
       const year = date.getFullYear();
 
       const hour = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
@@ -37,8 +37,8 @@ export class DateTimePickerComponent {
     if (!date) {
     console.log('no Date')
       const today = new Date();
-      const day = today.getDate() < 9 ? '0' + (today.getDate()) : today.getDate();
-      const month = today.getMonth() < 9 ? '0' + (today.getMonth() + 1) : today.getMonth() + 1;
+      const day = today.getDate() < 10 ? '0' + (today.getDate()) : today.getDate();
+      const month = today.getMonth() < 10 ? '0' + (today.getMonth() + 1) : today.getMonth() + 1;
       const year = today.getFullYear();
       this.pickedDate = `${year}-${month}-${day} ${time}`;
       this.dateInput.nativeElement.value = `${year}-${month}-${day}`;
