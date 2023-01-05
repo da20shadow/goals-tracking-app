ba5ccd47e9707aa55643216d6ee1b949f46b7ae1
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-05 14:28:17 +0200

    Modal will shows only on PC on mobile will redirect to page

--- resources/frontend/src/app/agenda/ui/agenda.component.ts
+++ resources/frontend/src/app/agenda/ui/agenda.component.ts
@@ -1,6 +1,6 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
 import {Observable} from "rxjs";
-import { Task } from 'src/app/core/models';
+import {Task, TaskPriority} from 'src/app/core/models';
 import {Store} from "@ngrx/store";
 import {agendaSelectors} from "../../Store/agenda-store/agenda-selectors";
 import {AgendaPageActions} from "../../Store/agenda-store/agenda-page.actions";
@@ -8,6 +8,12 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
 import {ModalService} from "../../core/services/modal.service";
 import {AddTaskModalComponent} from "../../shared/components/tasks-list/add-task-modal/add-task-modal.component";
 import {ViewTaskModalComponent} from "../../shared/components/tasks-list/view-task-modal/view-task-modal.component";
+import {TaskAPIActions} from "../../Store/task-store/task-api.actions";
+import {Operations} from "../../shared/enums/Operations";
+import {TaskService} from "../../tasks/services/task.service";
+import {NotificationService} from "../../core/services/notification.service";
+import {AgendaAPIActions} from "../../Store/agenda-store/agenda-api.actions";
+import {DayHours} from "../../shared/constants/DayHours";
 
 @Component({
   selector: 'app-agenda',
@@ -37,34 +43,9 @@ export class AgendaComponent {
   hourNow!: string;
   lineTopPx!: string;
   @ViewChild('calendarView') calendarView!: ElementRef;
-  times = [
-    {hour: '00:00'},
-    {hour: '01:00'},
-    {hour: '02:00'},
-    {hour: '03:00'},
-    {hour: '04:00'},
-    {hour: '05:00'},
-    {hour: '06:00'},
-    {hour: '07:00'},
-    {hour: '08:00'},
-    {hour: '09:00'},
-    {hour: '10:00'},
-    {hour: '11:00'},
-    {hour: '12:00'},
-    {hour: '13:00'},
-    {hour: '14:00'},
-    {hour: '15:00'},
-    {hour: '16:00'},
-    {hour: '17:00'},
-    {hour: '18:00'},
-    {hour: '19:00'},
-    {hour: '20:00'},
-    {hour: '21:00'},
-    {hour: '22:00'},
-    {hour: '23:00'},
-  ]
+  times = DayHours;
 
-  constructor(private store$: Store, private modalService: ModalService) {
+  constructor(private taskService: TaskService,private store$: Store, private modalService: ModalService,private notificationService: NotificationService) {
     this.todayTasks$ = this.store$.select(agendaSelectors.selectTodayTasks);
     this.overdueTasks$ = this.store$.select(agendaSelectors.selectOverdueTasks);
     this.nextTasks$ = this.store$.select(agendaSelectors.selectNextTasks);
@@ -102,8 +83,37 @@ export class AgendaComponent {
   addNewTask(){
     const modalRef = this.modalService.openFormModal(AddTaskModalComponent);
     modalRef.afterClosed().subscribe(result => {
-      console.log(result);
-      alert('COMING SOON...');
+      const taskForm = result.data && result.data.taskForm && result.data.taskForm.value ? result.data.taskForm.value : null;
+      if (taskForm){
+        let task = {};
+        task = {...task,title: taskForm.title}
+        //TODO: optimize the code make this reusable in separate file!
+        if (taskForm.priority !== ""){task = {...task,priority: taskForm.priority}}
+        if (taskForm.status !== ""){task = {...task,status: taskForm.status}}
+        if (taskForm.start_date !== ""){task = {...task,start_date: taskForm.start_date}}
+        if (taskForm.end_date !== ""){task = {...task,end_date: taskForm.end_date}}
+
+        this.taskService.saveTask(task).subscribe({
+          next: (response) => {
+            this.notificationService.showSuccessNotification(response.message);
+            const task = response.task;
+            console.log('response.task:',task)
+            let startDate;
+            if (task.start_date){startDate = new Date(task.start_date);}
+            let endDate;
+            if (task.end_date){endDate = new Date(task.end_date);}
+            const today = new Date();
+            if (startDate && startDate.getDate() === today.getDate() && startDate.getMonth() === today.getMonth() && startDate.getFullYear() === today.getFullYear()
+              || endDate && endDate.getDate() === today.getDate() && endDate.getMonth() === today.getMonth() && endDate.getFullYear() === today.getFullYear()){
+              this.store$.dispatch(AgendaAPIActions.addTodayTaskSuccess({task}));
+              if (task.priority === TaskPriority.URGENT){
+                this.store$.dispatch(TaskAPIActions.addUrgentTaskSuccess({task}))
+              }
+            }
+          },
+          error: (err) => this.notificationService.showErrorNotification(err.error.message)
+        })
+      }
     })
     //TODO: open modal for adding new task with option to select goal and target
   }
