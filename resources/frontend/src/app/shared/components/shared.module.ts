cac989ecfcb8776adccc93e93f55cd423f5c0aef
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-08 00:04:51 +0200

    created pipe

--- resources/frontend/src/app/shared/components/shared.module.ts
+++ resources/frontend/src/app/shared/components/shared.module.ts
@@ -1,17 +1,21 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PageTitleComponent} from "./page-elements/page-title/page-title.component";
+import {TaskTimePipe} from "../pipes/task-time.pipe";
 
 
 @NgModule({
   declarations: [
-    PageTitleComponent
+    PageTitleComponent,
+    TaskTimePipe
+
   ],
   imports: [
     CommonModule,
   ],
   exports: [
-    PageTitleComponent
+    PageTitleComponent,
+    TaskTimePipe
   ],
 })
 export class SharedModule {
