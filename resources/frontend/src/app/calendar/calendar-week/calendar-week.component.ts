be935be14dc469691a2d75fca46303bb187b018f
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-09 13:47:06 +0200

    added methods for calculating task position

--- resources/frontend/src/app/calendar/calendar-week/calendar-week.component.ts
+++ resources/frontend/src/app/calendar/calendar-week/calendar-week.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {DayHours} from "../../shared/constants/DayHours";
+import {Observable} from "rxjs";
+import {Store} from "@ngrx/store";
+import {agendaSelectors} from "../../Store/agenda-store/agenda-selectors";
+import {AgendaPageActions} from "../../Store/agenda-store/agenda-page.actions";
+import {Task} from 'src/app/core/models';
+import {ViewTaskModalComponent} from "../../shared/components/tasks-list/view-task-modal/view-task-modal.component";
+import {ModalService} from "../../core/services/modal.service";
 
 @Component({
   selector: 'app-calendar-week',
@@ -11,8 +18,11 @@ export class CalendarWeekComponent {
   interval: any;
   hourNow!: string;
   lineTopPx!: string;
+  todayTasks$: Observable<Task[]>;
 
-  constructor() {
+  constructor(private store$: Store,private modalService: ModalService) {
+    this.store$.dispatch(AgendaPageActions.getTodayTasks());
+    this.todayTasks$ = this.store$.select(agendaSelectors.selectTodayTasks);
     let date = new Date();
     this.hourNow = `${date.getHours()}:${String(date.getMinutes()).padStart(2, "0")}`
     this.lineTopPx = `${(date.getHours() * 60) + date.getMinutes()}px`;
@@ -28,5 +38,28 @@ export class CalendarWeekComponent {
       },60 *1000)
 
     }, secondsRemaining);
+  }
+
+  calculateTopPx(task: Task) {
+    if (task.start_date){
+      const startDate = new Date(task.start_date);
+      return (startDate.getHours() * 60) + startDate.getMinutes();
+    }
+    return null;
+  }
+
+  calculateTaskHeight(task: Task) {
+    if (task.start_date && task.end_date){
+      const startDate = new Date(task.start_date);
+      const startPx = (startDate.getHours() * 60) + startDate.getMinutes();
+      const endDate = new Date(task.end_date);
+      const endPx = (endDate.getHours() * 60) + endDate.getMinutes();
+      return (endPx - startPx);
+    }
+    return null;
+  }
+
+  openViewTaskModal(task: Task) {
+    this.modalService.openFormModal(ViewTaskModalComponent,{task})
   }
 }
