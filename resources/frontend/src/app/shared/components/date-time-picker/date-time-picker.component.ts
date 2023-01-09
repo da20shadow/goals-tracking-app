aee96e6d80efe78ed2f60415c51b1fdf997a9e16
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-09 13:47:29 +0200

    fixed bug in timepicker

--- resources/frontend/src/app/shared/components/date-time-picker/date-time-picker.component.ts
+++ resources/frontend/src/app/shared/components/date-time-picker/date-time-picker.component.ts
@@ -40,8 +40,8 @@ export class DateTimePickerComponent {
   }
 
   dateToString(date: any){
-    const day = date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate();
-    const month = date.getMonth() < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
+    const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+    const month = date.getMonth() < 9 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
     const year = date.getFullYear();
     return `${year}-${month}-${day}`;
   }
