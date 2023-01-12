97ff57b71275995d19b4d3435044606685fc5762
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-12 11:56:28 +0200

    added animations and created class variables

--- resources/frontend/src/app/calendar/ui/calendar/calendar.component.ts
+++ resources/frontend/src/app/calendar/ui/calendar/calendar.component.ts
@@ -1,46 +1,128 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TaskService} from "../../../tasks/services/task.service";
+import {Task} from "../../../core/models";
+import {ViewTaskModalComponent} from "../../../shared/components/tasks-list/view-task-modal/view-task-modal.component";
+import {ModalService} from "../../../core/services/modal.service";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.scss']
+  styleUrls: ['./calendar.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({
+        transform: 'scale(0.01)', opacity: 0
+      })),
+      transition('void <=> *', animate(400)),
+    ]),
+  ]
 })
 export class CalendarComponent {
-  currentMonth: string;
+  currentMonthTasksList: any[] = [];
   monthDays: any = [];
   prevMonthLastDays: any = [];
   nextMonthFirstDays: any = [];
+  date = new Date();
+  currentYear: any = this.date.getFullYear();
+  currentMonth: any = this.date.getMonth();
+  currentDate: any = this.date.getDate();
+  monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-  constructor() {
-    const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-    let date = new Date(),
-      firstDayOfTheCurrentMonth = new Date(date.getFullYear(),date.getMonth(), 1).getDay(),
-      lastDayOfCurrentMonth = new Date(date.getFullYear(),date.getMonth() + 1,0).getDate();
+  constructor(private taskService: TaskService,private modalService: ModalService) {
+    this.createCalendar()
+  }
 
-    this.currentMonth = months[date.getMonth()];
+  createCalendar(monthToShow?: string) {
 
-    const previewsMonthLastDay = date.getMonth() === 0
-      ? new Date(date.getFullYear() - 1,12, 0).getDate()
-      : new Date(date.getFullYear(), date.getMonth(),0).getDate();
+    if (monthToShow === 'next') {
+      this.currentMonth++;
+      if (this.currentMonth > 11) {
+        this.currentMonth = 0;
+        this.currentYear++;
+      }
+    } else if (monthToShow === 'prev') {
+      this.currentMonth--;
+      if (this.currentMonth < 0) {
+        this.currentMonth = 11;
+        this.currentYear--;
+      }
+    }
+
+    let firstDayOfTheCurrentMonth = new Date(this.currentYear, this.currentMonth, 1).getDay(),
+      lastDayOfCurrentMonth = new Date(this.currentYear, this.currentMonth + 1, 0).getDate();
+
+
+    const previewsMonthLastDay = this.currentMonth === 0
+      ? new Date(this.currentYear - 1, 12, 0).getDate()
+      : new Date(this.currentYear, this.currentMonth, 0).getDate();
 
     firstDayOfTheCurrentMonth = firstDayOfTheCurrentMonth === 0 ? 7 : firstDayOfTheCurrentMonth
-    if (firstDayOfTheCurrentMonth > 1){
+    if (firstDayOfTheCurrentMonth > 1) {
       const prevMonthDays = previewsMonthLastDay - (firstDayOfTheCurrentMonth - 1);
       for (let i = previewsMonthLastDay; i > prevMonthDays; i--) {
         this.prevMonthLastDays.unshift(i);
       }
     }
 
-    for (let i = 1; i < lastDayOfCurrentMonth + 1; i++) {
+    for (let i = 1; i <= lastDayOfCurrentMonth; i++) {
       this.monthDays.push(i);
     }
 
     const filledBoxes = this.prevMonthLastDays.length + this.monthDays.length;
-    if (filledBoxes > 35){
+    if (filledBoxes > 35) {
       const availableBoxes = 42 - filledBoxes;
       for (let i = 1; i <= availableBoxes; i++) {
         this.nextMonthFirstDays.push(i)
       }
+    } else if (filledBoxes >= 30) {
+      const availableBoxes = 35 - filledBoxes;
+      for (let i = 1; i <= availableBoxes; i++) {
+        this.nextMonthFirstDays.push(i)
+      }
     }
+    this.getCurrentMonthTasks(this.currentYear, this.currentMonth + 1);
+  }
+
+  getCurrentMonthTasks(year: number, month: number) {
+    this.taskService.getTasksByMonth(year, month).subscribe({
+      next: (tasks) => {
+        console.log(tasks)
+        tasks.forEach(t => {
+          if (t.start_date) {
+            const taskDate = new Date(t.start_date);
+            this.currentMonthTasksList.push({position: taskDate.getDate(), task: t})
+          }
+        })
+      },
+      error: (err) => {
+
+      }
+    })
+  }
+
+  openViewTaskModal(task: Task) {
+    this.modalService.openFormModal(ViewTaskModalComponent,{task})
+  }
+
+  prevMonth() {
+    this.clearCalendar();
+    this.createCalendar('prev');
+  }
+
+  nextMonth() {
+    this.clearCalendar();
+    this.createCalendar('next');
+  }
+
+  clearCalendar() {
+    this.currentMonthTasksList = [];
+    this.monthDays = [];
+    this.prevMonthLastDays = [];
+    this.nextMonthFirstDays = [];
+  }
+
+  addNewTask() {
+    alert('Add New Task clicked!')
   }
 }
