e69391ed82e50c1fc5f69cadc85691aa207db9be
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-01-12 11:55:52 +0200

    shared module moved to main shared folder

--- resources/frontend/src/app/shared/shared.module.ts
+++ resources/frontend/src/app/shared/shared.module.ts
@@ -1,13 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {PageTitleComponent} from "./page-elements/page-title/page-title.component";
-import {TaskTimePipe} from "../pipes/task-time.pipe";
+import {PageTitleComponent} from "./components/page-elements/page-title/page-title.component";
+import {TaskTimePipe} from "./pipes/task-time.pipe";
+import {ShortTitlePipe} from "./pipes/short-title.pipe";
 
 
 @NgModule({
   declarations: [
     PageTitleComponent,
     TaskTimePipe,
+    ShortTitlePipe,
+
   ],
   imports: [
     CommonModule,
@@ -15,6 +18,7 @@ import {TaskTimePipe} from "../pipes/task-time.pipe";
   exports: [
     PageTitleComponent,
     TaskTimePipe,
+    ShortTitlePipe,
   ],
 })
 export class SharedModule {
